2e172a575ca7518f08b38e7cdcfcb8e705f23cd1
Bogdan Stefan Sandoiu B.Sandoiu@student.rug.nl <b.sandoiu@student.rug.nl>
2025-07-11 16:50:46 +0200

    created GRU model and added power lag to the preprocessing, and got the highet correlated features to the power

--- project.sh
+++ project.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=03:20:00         
+#SBATCH --time=06:20:00         
 #SBATCH --partition=gpu         
 #SBATCH --gres=gpu:1            
-#SBATCH --mem=32000              
+#SBATCH --mem=64000              
 
 # Load required modules
 module load Python/3.10.4-GCCcore-11.3.0
@@ -15,6 +15,6 @@ source /home2/s5549329/windAI_rug/venv/bin/activate
 python --version
 
 # Run your Python project
-python /home2/s5549329/windAI_rug/WindAi/deep_learning/preprocessing/preprocess_windowing_region.py
+python /home2/s5549329/windAI_rug/WindAi/deep_learning/models/RNN.py
 
 deactivate
